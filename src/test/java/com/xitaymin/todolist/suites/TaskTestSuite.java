73923b8babe58984d20ea9937d2e092c6083bf6a
xitaymin <y.barashenko@gmail.com>
2021-10-24 16:32:38 +0300

    test if task list is empty at first

--- src/test/java/com/xitaymin/todolist/suites/TaskTestSuite.java
+++ src/test/java/com/xitaymin/todolist/suites/TaskTestSuite.java
@@ -1,6 +1,18 @@
 package com.xitaymin.todolist.suites;
 
-public class TaskTestSuite extends BaseTestSuite {
+import org.junit.jupiter.api.Test;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+public class TaskTestSuite extends BaseTestSuite
+{
+    @Test
+    void isTaskListEmptyBeforeCreationTest() throws Exception
+    {
+        mockMvc.perform(get("/todo").accept(APPLICATION_JSON)).andExpect(status().isOk())
+            .andExpect(content().json("[]"));
+    }
 }
